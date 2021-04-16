b78f9e35f13e52481a6687fd096986adffa40f96
Vishal Mahalingam <vishal.mahalingam@rrd.com>
2021-04-16 08:04:11 +0530

    1604

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -8,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
 export class RestaurantComponent implements OnInit {
 
   constructor() { }
+  err=true;
+  getvalues(val){
+    alert(val);
+
+  }
+  info=[{priority:"5",time:"testme"},
+  {priority:"15",time:"testme"},
+  {priority:"10",time:"testme"},
+  {priority:"21",time:"testme"}
+]
+
+getClass(priority){
+  
+  return {'message': priority < 6,
+                'warn': priority > 10 && priority <20, 
+                'error fa fa-exclamation-triangle': priority > 20}
+
+}
 
   ngOnInit(): void {
   }
   popname ='dhoni';
+
+  allrestaurant=[
+{name:"aaa",price:"350 for 2",offer:"20%",time:"30mins"},
+{name:"bbb",price:"350 for 2",offer:"20%",time:"30mins"},
+{name:"ccc",price:"350 for 2",offer:"20%",time:"30mins"},
+{name:"ddd",price:"350 for 2",offer:"20%",time:"30mins"},
+{name:"ddd",price:"350 for 2",offer:"20%",time:"30mins"},
+{name:"ddd",price:"350 for 2",offer:"20%",time:"30mins"},
+{name:"ddd",price:"350 for 2",offer:"20%",time:"30mins"},
+
+{name:"ddd",price:"350 for 2",offer:"20%",time:"30mins"},
+
+{name:"eee",price:"350 for 2",offer:"20%",time:"30mins"}
+
+
+  ]
+
   data=[{name:'copper kitchen',place:'chennai'},
   {name:'SS hydrabad',place:'chennai'},
   {name:'Ambur ',place:'chennai'},
--- src/app/restaurant/restaurant.module.ts
+++ src/app/restaurant/restaurant.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { RestaurantRoutingModule } from './restaurant-routing.module';
 import { RestaurantComponent } from './restaurant.component';
-
+import {FormsModule} from '@angular/forms'
+import{NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [
     RestaurantComponent
   ],
   imports: [
-    CommonModule,
-    RestaurantRoutingModule
+    CommonModule,FormsModule,
+    RestaurantRoutingModule,NgbModule
   ],
   exports:[
     RestaurantComponent
